6d501a44dd3817f0ef2fe80628f31f03fe6bd335
hyemin23 <hm_stom@naver.com>
2020-08-17 18:11:08 +0900

    # ROLE_권한별 게시글 등록 여부 분기

--- src/main/java/com/jojoldu/book/springboot/web/IndexController.java
+++ src/main/java/com/jojoldu/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.jojoldu.book.springboot.web;
 
+import com.jojoldu.book.springboot.config.auth.dto.SessionUser;
 import com.jojoldu.book.springboot.service.posts.PostsService;
 import com.jojoldu.book.springboot.web.dto.PostsListResponseDto;
 import com.jojoldu.book.springboot.web.dto.PostsResponseDto;
@@ -11,6 +12,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -20,15 +22,23 @@ public class IndexController {
 
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
+
 
     @GetMapping("/")
     public String home(Model model){
         List<PostsListResponseDto> allDesc = postsService.findAllDesc();
         model.addAttribute("posts",allDesc);
 
+
+        //구글 로그인 성공 시 user로 가져올 수 있음.
+        SessionUser user = (SessionUser)httpSession.getAttribute("user");
+        if(user != null){
+            model.addAttribute("userName",user.getName());
+        }
+
         //머스테치 스타터 덕분에 컨트롤러에서 문자열을 반환할 때 앞의 경로외
         //뒤의 파일 확장자는 자동으로 지정됩니다. ( /template ) 까지
-
          return "index";
     }
     @GetMapping("/posts/save")
@@ -45,4 +55,7 @@ public class IndexController {
         model.addAttribute("post",byId);
         return "posts-update";
     }
+
+
+
 }
